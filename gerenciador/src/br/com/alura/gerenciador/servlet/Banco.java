17fe25dd0513205873ebff533d9a887d27072c45
Wesley <wesley.contato@hotmail.com>
2020-10-20 20:19:37 -0300

    Removendo empresa

--- gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,17 +1,23 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import org.apache.jasper.tagplugins.jstl.core.ForEach;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
 
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
 
 		lista.add(empresa);
@@ -19,6 +25,7 @@ public class Banco {
 	}
 
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 
 	}
@@ -27,4 +34,16 @@ public class Banco {
 		return Banco.lista;
 	}
 
+	public void removeEmpresa(Integer id) {
+
+		Iterator<Empresa> it = lista.iterator();
+
+		while (it.hasNext()) {
+			Empresa emp = it.next();
+
+			if (emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
 }
